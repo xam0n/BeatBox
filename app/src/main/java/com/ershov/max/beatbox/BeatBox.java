cd69286ccbd38337490fb2f2f2d7864c92d37c14
MAX-PC\Max <xamon_ne@mail.ru>
2016-10-23 02:18:02 +0300

    1 part

---
+++ app/src/main/java/com/ershov/max/beatbox/BeatBox.java
@@ -0,0 +1,47 @@
+package com.ershov.max.beatbox;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Max on 23.10.2016.
+ */
+
+public class BeatBox {
+    private static final String TAG = "BeatBox";
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+    private List<Sound> mSounds = new ArrayList<>();
+
+    private AssetManager mAssets;
+
+    public BeatBox (Context context) {
+        mAssets = context.getAssets();
+        loadSounds();
+    }
+
+    private void loadSounds() {
+        String[] soundNames;
+        try {
+            soundNames = mAssets.list(SOUNDS_FOLDER);
+            Log.i(TAG, "Found " + soundNames.length + " sounds");
+        } catch (IOException ioe) {
+            Log.e(TAG, "Could not list assets", ioe);
+            return;
+        }
+
+        for (String filename : soundNames) {
+            String assetPath = SOUNDS_FOLDER + "/" + filename;
+            Sound sound = new Sound(assetPath);
+            mSounds.add(sound);
+        }
+    }
+
+    public List<Sound> getSounds() {
+        return mSounds;
+    }
+}
---
+++ app/src/main/java/com/ershov/max/beatbox/Sound.java
@@ -0,0 +1,25 @@
+package com.ershov.max.beatbox;
+
+/**
+ * Created by Max on 23.10.2016.
+ */
+
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace("wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
